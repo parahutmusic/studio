92f9eeeac6b3eeb9d812eaeb0cdbba6561151d43
parahutmusic <echangzt1414@gmail.com>
2017-08-22 14:07:14 +0700

    about

--- wg/menu.php
+++ wg/menu.php
@@ -1,7 +1,7 @@
 
 		<nav id="nav">
 			<a href="index.php" class="icon fa-home"><span>หน้าแรก</span></a>
-			<a href="portfolio.php" class="icon fa-folder"><span>เกี่ยวกับเรา</span></a>
+			<a href="about.php" class="icon fa-folder"><span>เกี่ยวกับเรา</span></a>
 			<a href="contact.php" class="icon fa-envelope"><span>ติดต่อ</span></a>
 			<a href="https://www.facebook.com/%E0%B8%9E%E0%B8%B2%E0%B8%A3%E0%B8%B2%E0%B8%AE%E0%B8%B1%E0%B8%97-%E0%B8%A1%E0%B8%B4%E0%B8%A7%E0%B8%AA%E0%B8%B4%E0%B8%84-%E0%B9%82%E0%B8%A3%E0%B8%87%E0%B9%80%E0%B8%A3%E0%B8%B5%E0%B8%A2%E0%B8%99%E0%B9%83%E0%B8%95%E0%B9%89%E0%B9%82%E0%B8%84%E0%B8%99%E0%B8%A2%E0%B8%B2%E0%B8%87-220731641380364/" class="icon fa-facebook-official"><span>Facebook</span></a>
 			<a href="https://www.youtube.com/user/Parahutmusic" class="icon fa-youtube-square "><span>Youtube</span></a>
